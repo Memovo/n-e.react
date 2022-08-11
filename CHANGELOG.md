a55c9165957f3e2e240375909ace57d006d6ea6d
Memovor <liangmr0505@163.com>
2022-08-11 15:16:46 +0800

    feat: add commit check

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+## 0.1.0 (2022-08-11)
+
+* test ([dfc5f10](https://github.com/Memovo/q-k.react/commit/dfc5f10))
+* test ([750f4d6](https://github.com/Memovo/q-k.react/commit/750f4d6))
+* test ([a6c2ac7](https://github.com/Memovo/q-k.react/commit/a6c2ac7))
+* feat: add commit check ([d75e151](https://github.com/Memovo/q-k.react/commit/d75e151))
+* feat: add commit check ([5b81550](https://github.com/Memovo/q-k.react/commit/5b81550))
+* feat: add commit check ([0260c5b](https://github.com/Memovo/q-k.react/commit/0260c5b))
+* feat: init project ([36b2207](https://github.com/Memovo/q-k.react/commit/36b2207))
+
+
+
---
+++ commitlint.config.js
@@ -0,0 +1,21 @@
+'use strict'
+
+module.exports = {
+    // 继承的规则
+    extends: ['@commitlint/config-conventional'],
+    // 定义规则类型
+    rules: {
+        // type 类型定义，表示 git 提交的 type 必须在以下类型范围内
+        rules: {
+            'type-enum': [
+                2,
+                'always',
+                ['WIP', 'feat', 'fix', 'refactor', 'docs', 'test', 'style', 'chore', 'revert'],
+            ],
+            'type-case': [1, 'always', ['lower-case', 'upper-case']],
+            'scope-case': [0, 'never'],
+            'subject-case': [0, 'never'],
+            'scope-empty': [0, 'never'],
+        },
+    },
+}
