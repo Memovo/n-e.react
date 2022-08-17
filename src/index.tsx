8fc077fc62c4eb0b656a206280dd28fb5f30169c
Memovor <liangmr0505@163.com>
2022-08-17 18:16:01 +0800

    fix: 入口文件调整

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './styles/index.scss'
 import Main from './main'
+import { TodoList } from './stores/todo-list'
+import { StoreProvider } from "./helpers/store-provider"
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-)
+const todoList = new TodoList([
+  'Should Starting Writing in React',
+  'Should Learn MobX',
+  'Should Watch Once Piece :)'
+])
+
+//@ts-ignore - for debugging
+window.todoList = todoList
+
 // React.StrictMode最佳实践: https://reactjs.org/docs/strict-mode.html
-root.render(
-  <>
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StoreProvider value={todoList}>
     <Main />
-  </>
+  </StoreProvider>
 )
