194820c8303a49de6e18de0ddb397d4eac1f4885
Memovor <liangmr0505@163.com>
2022-08-25 17:22:14 +0800

    feat: 新增逻辑完善

--- src/components/todo-new/index.tsx
+++ src/components/todo-new/index.tsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import { Button, Input } from 'antd'
 import '../../styles/todo-new.scss'
+import { useStore } from "../../helpers/use-store"
+import { onEnterPress } from "../../helpers/use-enter"
 
 const TodoNew = () => {
+    const [newTodo, setTodo] = React.useState('')
+
+    const todoList = useStore()
 
     const addTodo = () => {
-        console.log('add')
+        todoList.addTodo(newTodo)
+        setTodo('')
     }
 
     return (
         <div className="todo-new">
-            <Input type="text" style={{ flex: 1, marginRight: 10 }} />
+            <Input
+                type="text"
+                value={newTodo}
+                style={{ flex: 1, marginRight: 10 }}
+                onKeyDown={onEnterPress(addTodo)}
+                onChange={e => setTodo(e.target.value)}
+            />
             <Button style={{ width: 80 }} onClick={addTodo}>Add Todo</Button>
         </div>
     )
